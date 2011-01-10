c47ae1d17d21a57821306c584c4d87c6ee852c2d
Christian Lohmaier <lohmaier+github@googlemail.com>
2011-01-10 02:49:17 +0100

    add Slovenian translation for cmsworkflow and custom pages

---
+++ mysite/lang/sl_SI.php
@@ -0,0 +1,9 @@
+<?php
+global $lang;
+
+$lang['sl_SI']['Downloadpage']['BITTORRENT'] = 'Prenesi z BitTorrentom';
+$lang['sl_SI']['Downloadpage']['DLSOURCE']   = 'Prenesite izvorno kodo in sami izdelajte namestitveni paket';
+$lang['sl_SI']['Downloadpage']['DLSDK']      = 'Prenesite SDK za razvijanje razširitev in zunanjih orodij';
+$lang['sl_SI']['Downloadpage']['DLSUITE']    = 'Drugi načini prenosa pisarniškega paketa LibreOffice';
+
+$lang['sl_SI']['Donationpage']['AMOUNT'] = 'Znesek';
