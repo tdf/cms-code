4c3ed98366bac8b91377cbb19e48a04ad0f6caa3
Christian Lohmaier <lohmaier+github@googlemail.com>
2011-01-17 16:36:19 +0100

    hungarian translations for download-page

---
+++ mysite/lang/hu_HU.php
@@ -0,0 +1,7 @@
+<?php
+global $lang;
+
+$lang['hu_HU']['Downloadpage']['BITTORRENT'] = 'Letöltés BitTorrent segítségével';
+$lang['hu_HU']['Downloadpage']['DLSOURCE']   = 'A forráskód letöltése saját telepítőkészlet készítéséhez';
+$lang['hu_HU']['Downloadpage']['DLSDK']      = 'Az SDK letöltése kiterjesztések és külső eszközök fejlesztéséhez';
+$lang['hu_HU']['Downloadpage']['DLSUITE']    = 'Alternatív letöltési linkek a LibreOffice csomagjaihoz';
