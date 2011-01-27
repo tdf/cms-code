7c070c864217919c5507c4e94035e53187566295
Christian Lohmaier <lohmaier+github@googlemail.com>
2011-01-27 23:24:28 +0100

    add Finnish translations

---
+++ mysite/lang/fi_FI.php
@@ -0,0 +1,10 @@
+<?php
+global $lang;
+
+$lang['fi_FI']['Downloadpage']['BITTORRENT'] = 'Lataa BitTorrent-verkosta';
+$lang['fi_FI']['Downloadpage']['DLSOURCE']   = 'Lataa ohjelmiston lähdekoodi';
+$lang['fi_FI']['Downloadpage']['DLSDK']      = 'Lataa kehitystyökalut (SDK) lisäosien ja apuohjelmien kehittämiseksi';
+$lang['fi_FI']['Downloadpage']['DLSUITE']    = 'Vaihtoehtoinen tapa valita ladattava LibreOffice-paketti';
+$lang['fi_FI']['Downloadpage']['INSTALLER']  = 'asennuspaketti';
+
+$lang['fi_FI']['Navigation']['GOTOPAGE'] = '"Siirry sivulle %s"';
