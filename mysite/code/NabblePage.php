a01fde4c5307ade5d96f2e1e0e70ee3b498a7598
Christian Lohmaier <lohmaier+github@googlemail.com>
2011-02-09 18:24:34 +0100

    allow to specify different forum to initially display on NabblePage

--- mysite/code/NabblePage.php
+++ mysite/code/NabblePage.php
@@ -2,6 +2,17 @@
 /**
  */
 class NabblePage extends Page {
+	public static $db = array(
+		'NabbleForum' => 'varchar(7)',
+	);
+	static $defaults = array(
+		'NabbleForum' => '1816769',
+	);
+	function getCMSFields() {
+		$fields = parent::getCMSFields();
+		$fields->addFieldToTab('Root.Content.Main', new TextField('NabbleForum', "Specify the Nabble-Forum ID to display initially (only the number)"), 'Content');
+		return $fields;
+	}
 }
 
 class NabblePage_Controller extends Page_Controller {
