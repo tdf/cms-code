28a71c0b068dbc0ae547c33426e086119b31adc3
Christian Lohmaier <lohmaier+github@googlemail.com>
2011-02-10 02:45:22 +0100

    Lithuanian translations

---
+++ mysite/lang/lt_LT.php
@@ -0,0 +1,12 @@
+<?php
+global $lang;
+
+$lang['lt_LT']['Downloadpage']['BITTORRENT'] = 'siųstis naudojant „BitTorrent“';
+$lang['lt_LT']['Downloadpage']['DLSOURCE'] = 'Parsisiųsti „LibreOffice“ pradinius tekstus (programuotojams)';
+$lang['lt_LT']['Downloadpage']['DLSDK'] = 'Parsisiųsti programavimo bibliotekas (SDK) plėtinių ir papildomų priemonių kūrimui';
+$lang['lt_LT']['Downloadpage']['DLSUITE'] = 'Kitas būdas parsisiųsti „LibreOffice“ raštinės programų paketą';
+$lang['fi_FI']['Downloadpage']['INSTALLER'] = 'diegimo programa';
+
+$lang['lt_LT']['Donationpage']['AMOUNT'] = 'Paramos suma:';
+
+$lang['lt_LT']['Navigation']['GOTOPAGE'] = '"Eiti į tinklalapį „%s“"';
