4a68dc21f4ab61e89e910b877ab11c0edb986452
Christian Lohmaier <lohmaier+github@googlemail.com>
2011-04-12 20:12:50 +0200

    fi_FI → lt_LT in translation file for lt

--- mysite/lang/lt_LT.php
+++ mysite/lang/lt_LT.php
@@ -5,7 +5,7 @@ $lang['lt_LT']['Downloadpage']['BITTORRENT'] = 'siųstis naudojant „BitTorrent
 $lang['lt_LT']['Downloadpage']['DLSOURCE'] = 'Parsisiųsti „LibreOffice“ pradinius tekstus (programuotojams)';
 $lang['lt_LT']['Downloadpage']['DLSDK'] = 'Parsisiųsti programavimo bibliotekas (SDK) plėtinių ir papildomų priemonių kūrimui';
 $lang['lt_LT']['Downloadpage']['DLSUITE'] = 'Kitas būdas parsisiųsti „LibreOffice“ raštinės programų paketą';
-$lang['fi_FI']['Downloadpage']['INSTALLER'] = 'diegimo programa';
+$lang['lt_LT']['Downloadpage']['INSTALLER'] = 'diegimo programa';
 
 $lang['lt_LT']['Donationpage']['AMOUNT'] = 'Paramos suma:';
 
