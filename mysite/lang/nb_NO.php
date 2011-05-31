79126a81023ff6582ab2779feeef1661d081de28
Christian Lohmaier <lohmaier+github@googlemail.com>
2011-05-31 22:26:32 +0200

    Norwegian Bokmål translation for Downlaodpage (provided by Olav)

---
+++ mysite/lang/nb_NO.php
@@ -0,0 +1,7 @@
+<?php
+global $lang;
+
+$lang['nb_NO']['Downloadpage']['BITTORRENT'] = 'Last ned med BitTorrent';
+$lang['nb_NO']['Downloadpage']['DLSOURCE'] = 'Last ned kildekoden for å lage din egen installasjon';
+$lang['nb_NO']['Downloadpage']['DLSDK'] = 'Last ned SDK for å utvikle utvidelser og eksterne verktøy';
+$lang['nb_NO']['Downloadpage']['DLSUITE'] = 'Alternative nedlastingsmetoder for LibreOffice';
