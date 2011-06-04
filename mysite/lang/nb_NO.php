c2c7acba10eb51a371482d61924fe9e51747ee19
Christian Lohmaier <lohmaier+github@googlemail.com>
2011-06-04 17:54:33 +0200

    some more translations (by Olav)

--- mysite/lang/nb_NO.php
+++ mysite/lang/nb_NO.php
@@ -2,6 +2,10 @@
 global $lang;
 
 $lang['nb_NO']['Downloadpage']['BITTORRENT'] = 'Last ned med BitTorrent';
-$lang['nb_NO']['Downloadpage']['DLSOURCE'] = 'Last ned kildekoden for å lage din egen installasjon';
-$lang['nb_NO']['Downloadpage']['DLSDK'] = 'Last ned SDK for å utvikle utvidelser og eksterne verktøy';
-$lang['nb_NO']['Downloadpage']['DLSUITE'] = 'Alternative nedlastingsmetoder for LibreOffice';
+$lang['nb_NO']['Downloadpage']['DLSOURCE']   = 'Last ned kildekoden for å lage din egen installasjon';
+$lang['nb_NO']['Downloadpage']['DLSDK']      = 'Last ned SDK for å utvikle utvidelser og eksterne verktøy';
+$lang['nb_NO']['Downloadpage']['DLSUITE']    = 'Alternative nedlastingsmetoder for LibreOffice';
+$lang['nb_NO']['Downloadpage']['INSTALLER']  = 'Installasjonspakke';
+
+$lang['nb_NO']['Navigation']['GOTOPAGE']   = '"Gå til %s"';
+$lang['nb_NO']['Navigation']['TOTOPTITLE'] = 'Gå til toppen';
