a8106faa7762fee18703df1fce9621ca925d7284
Christian Lohmaier <lohmaier+github@googlemail.com>
2011-06-04 03:06:44 +0200

    Spanish translation for dl-page (by Daniel)

---
+++ mysite/lang/es_ES.php
@@ -0,0 +1,8 @@
+<?php
+global $lang;
+
+$lang['es_ES']['Downloadpage']['BITTORRENT'] = 'Descargar usando BitTorrent';
+$lang['es_ES']['Downloadpage']['DETAILLINK'] = 'Obtener detalles (md5sum,…)';
+$lang['es_ES']['Downloadpage']['DLSOURCE']   = 'Descargar el código fuente para construir su propio instalador';
+$lang['es_ES']['Downloadpage']['DLSDK']      = 'Descargar el SDK para desarrollar extensiones y herramientas externas';
+$lang['es_ES']['Downloadpage']['DLSUITE']    = 'Otras maneras de descargar LibreOffice, la suite de productividad';
