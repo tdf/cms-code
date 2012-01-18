e99bc1e8e8d8d74eec77b8fb18d394b8bc556b58
Christian Lohmaier <lohmaier+github@googlemail.com>
2012-01-18 23:34:13 +0100

    add "master" translation file

---
+++ mysite/lang/en_US.php
@@ -0,0 +1,15 @@
+<?php
+global $lang;
+
+$lang['en_US']['Donationpage']['AMOUNT']='Amount:';
+
+$lang['en_US']['Downloadpage']['BITTORRENT']='Download using BitTorrent';
+$lang['en_US']['Downloadpage']['DETAILLINK']='Get details (md5sum,…)';
+$lang['en_US']['Downloadpage']['DLSDK']     ='Download the SDK for developing extensions and external tools';
+$lang['en_US']['Downloadpage']['DLSOURCE']  ='Download the source code to build your own installer';
+$lang['en_US']['Downloadpage']['DLSUITE']   ='Other way to download LibreOffice, the productivity suite';
+
+/* Localizers: this string HAS to be enclosed in either single or double ASCII quotes! It is a title attribute of a link. */
+$lang['en_US']['Navigation']['GOTOPAGE']  ='"Go to the %s page"';
+$lang['en_US']['Navigation']['TOTOPTITLE']='scroll to top of page';
+$lang['en_US']['Navigation']['TOTOP']     ='⬆ to top';
