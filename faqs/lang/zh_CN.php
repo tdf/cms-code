1c429b8ae9fb708025f8976be3c50d154ca9e17a
Dean Lee <xslidian@gmail.com>
2012-01-19 20:49:00 +0800

    Added the Chinese (Simplified) translation

---
+++ faqs/lang/zh_CN.php
@@ -0,0 +1,36 @@
+<?php
+global $lang;
+
+$lang['zh_CN']['FaqsPageEntry']['QUESTIONLABEL'] = '问题';
+$lang['zh_CN']['FaqsPageEntry']['ANSWERLABEL'] = '回答';
+$lang['zh_CN']['FaqsPageEntry']['COUNTHITSLABEL'] = '统计点击数?';
+$lang['zh_CN']['FaqsPageEntry']['HITSLABEL'] = '点击数';
+$lang['zh_CN']['FaqsPageEntry']['RELATEDLABEL'] = '相关问题';
+
+$lang['zh_CN']['FaqsHolder']['SEARCHTITLE'] = '搜索';
+$lang['zh_CN']['FaqsHolder']['SEARCHSELCATEGORY'] = '分类';
+$lang['zh_CN']['FaqsHolder']['SEARCHALLCATEGORIES'] = '所有分类';
+$lang['zh_CN']['FaqsHolder']['SEARCHBUTTONLABEL'] = 'Go';
+$lang['zh_CN']['FaqsHolder']['SEARCHRESULTS'] = '搜索结果';
+
+$lang['zh_CN']['FaqsHolder.ss']['GOTOFAQ'] = '显示文档';
+$lang['zh_CN']['FaqsHolder.ss']['VIEWFAQSCOMMENTS'] = '查看评论';
+$lang['zh_CN']['FaqsHolder.ss']['NOFAQSENTRIES'] = '没有 faq 条目';
+$lang['zh_CN']['FaqsHolder.ss']['GOTOPREVIOUSPAGE'] = '查看上一页';
+$lang['zh_CN']['FaqsHolder.ss']['GOTONEXTPAGE'] = '查看下一页';
+$lang['zh_CN']['FaqsHolder.ss']['PREVIOUSPAGE'] = '上一页';
+$lang['zh_CN']['FaqsHolder.ss']['NEXTPAGE'] = '下一页';
+$lang['zh_CN']['FaqsHolder.ss']['VIEWPAGENUM'] = '查看页码';
+$lang['zh_CN']['FaqsPage.ss']['RELATEDFAQ'] = '相关问题';
+
+$lang['zh_CN']['FaqsSearchResults.ss']['READMORE'] = '阅读全文... ';
+$lang['zh_CN']['FaqsSearchResults.ss']['NORESULTSFAQS'] = '抱歉，没有符合您给定条件的搜索结果。';
+
+$lang['zh_CN']['FaqsTopWidget']['TITLE'] = '常见问题';
+$lang['zh_CN']['FaqsTopWidget']['LIMIT'] = '要显示的 faq 数量';
+$lang['zh_CN']['FaqsTopWidget']['LABEL'] = '小工具标题';
+
+$lang['zh_CN']['FaqsSearchWidget']['TITLE'] = '搜索常见问题';
+$lang['zh_CN']['FaqsSearchWidget']['LIMIT'] = '每页条目数';
+$lang['zh_CN']['FaqsSearchWidget']['LABEL'] = '小工具标题';
+?>
